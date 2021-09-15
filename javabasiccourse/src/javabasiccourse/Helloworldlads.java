bb0e93e4347dcea1fce600d47c3a42b18fd4ccc4
sutanu2020 <chakrabortysutanu9@gmail.com>
2021-09-16 00:32:51 +0530

    First commit

---
+++ javabasiccourse/src/javabasiccourse/Helloworldlads.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package javabasiccourse;
+
+/**
+ * @author sutanu
+ *
+ */
+public class Helloworldlads {
+
+	/**
+	 * 
+	 */
+	public Helloworldlads() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("First line of github");
+
+	}
+
+}
